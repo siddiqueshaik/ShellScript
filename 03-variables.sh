5d708d5486fa2e9cbee74576ad715810e96ba041
DevOpsCloud <deepsiddique@gmail.com>
2024-06-14 09:42:13 -0700

    assigning variables

---
+++ 03-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hi $PERSON2"
+
+echo "$PERSON2 : Hello..... $PERSON1!!  how are you?"
+
+echo " $PERSON1 : I am good......How are you doing?"
+
+echo "$PERSON2 : I am great $PERSON1!"
+
+DATE=$(date)
+echo "DATE is ----- $DATE"
