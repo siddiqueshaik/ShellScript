755a77a5d2a6c64c171890797af17ccf94221e02
DevOpsCloud <deepsiddique@gmail.com>
2024-06-14 10:19:47 -0700

    assigning variables

--- install-mysql.sh
+++ install-mysql.sh
@@ -5,8 +5,12 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "YOU ARE NOT A ROOT USER"
+    exit 1
 else
      echo "ROOT USER"
 fi
 
+echo "Installing mysql---------------"
 yum install mysql -y
+
+echo "mysql installed"
