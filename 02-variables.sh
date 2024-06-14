6cbc272152d2060c1bc9478ffc7c27836e056e93
DevOpsCloud <deepsiddique@gmail.com>
2024-06-14 09:34:22 -0700

    assigning variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PERSON1="RAM"
-PERSON2="SURESH"
+PERSON1="SID"
+PERSON2="HANNU"
 
 echo "$PERSON1 : Hi $PERSON2"
 
-echo "$PERSON2 : Hello..... Ra$PERSON1!!  how are you?"
+echo "$PERSON2 : Hello..... $PERSON1!!  how are you?"
 
 echo " $PERSON1 : I am good......How are you doing?"
 
