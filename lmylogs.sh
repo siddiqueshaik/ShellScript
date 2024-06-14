e32b47a8187d7a7255c6069b6e162fd2c0990a81
DevOpsCloud <deepsiddique@gmail.com>
2024-06-14 11:22:18 -0700

    assigning variables

---
+++ lmylogs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+ID=$(id -u)
+DATE=$(date)
+
+LOGFILE="/tmp/$0-$DATE.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2------------------FAILED"
+        exit 1
+    else
+        echo "$2-------------------success"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "YOU ARE NOT A ROOT USER"
+    exit 1
+else
+     echo "ROOT USER"
+fi
+
+yum install mysql -y &>>$LOGFILE
+VALIDATE $? "INSTALLING MYSQL----------------------"
+
+yum install git -y &>>$LOGFILE
+VALIDATE $? "INSTALLING GIT------------------------"
