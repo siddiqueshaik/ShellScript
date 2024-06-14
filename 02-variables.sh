aa134521497d230ab4b1a8bd890ea812410cd48f
DevOpsCloud <deepsiddique@gmail.com>
2024-06-14 01:43:34 -0700

    variables commit

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Ramesh : Hi Suresh"
+
+echo "Suresh : Hello..... Ramesh!!  how are you?"
+
+echo " Ramesh : I am good......How are you doing?"
+
+echo "Suresh : I am great Ramesh!"
